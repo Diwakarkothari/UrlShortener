187e769ea84c3cbb9599de34f9d72d39159d4245
Diwakarkothari <diwakarkothari810@gmail.com>
2024-08-01 22:17:51 +0530

    Headers

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,11 +4,14 @@ async function restrictlogin(req,res,next)
 {
     // agar cookie hai to he uid read karega
     
-    const userid = req.cookies?.uid;
+    // const userid = req.cookies?.uid;
+    const userid = req.headers['Authorization'];
     if(!userid)
         return res.redirect('/login');
 
-    const user = getUser(userid);
+    const token = userid.split('Bearer')[1];
+    // const user = getUser(userid);
+    const user = getUser(token);
     if(!user)
         return res.redirect('/login');
     req.user = user;
